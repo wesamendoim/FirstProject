c84be42fdcc7fddbf9ed1b85d838feee90d5e244
wesamendoim <30731163+wesamendoim@users.noreply.github.com>
2023-07-26 18:36:53 +0000

    Aula de manipulação de dados e variaveis. CONVERSOES

--- main.py
+++ main.py
@@ -12,9 +12,20 @@ print("Teste")
 '''
 Meu segundo codigo 
 Aula Variáveis
-'''
-
 x = 4
 print(x + x)
 print(x + x)
 print(x + x)
+'''
+
+'''
+AULA DE FRASES 
+O Wesley tem 29 anos de idade e mora em São Paulo 
+'''
+nome = 'Wesley'
+idade = 32
+cidade = 'São Paulo'
+idade = str(idade)
+
+print('O ' + nome + ' tem ' + idade + ' anos de idade e mora em ' + cidade)
+
