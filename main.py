8e4c69b934cde1bb68e7183bd946ed976dc6431c
wesamendoim <30731163+wesamendoim@users.noreply.github.com>
2023-07-26 17:44:11 +0000

    Segunda Aula
    Aula de Variaveis

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ Nome: Wesley Valadão
 Data: 26/07/2023
 Versao 1.0
 '''
-# Meu primeiro codigo 
-
+'''
+Meu primeiro codigo 
 print("Wesley é o meu nome")
 print("Teste")
+''' 
+'''
+Meu segundo codigo 
+Aula Variáveis
+'''
+
+x = 4
+print(x + x)
+print(x + x)
+print(x + x)
