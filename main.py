0f69d8f235f2e37783f53f8b3429ce6b8d399e99
wesamendoim <30731163+wesamendoim@users.noreply.github.com>
2023-07-27 13:13:59 +0000

    Aula 17
    Usando a função a TYPE
    Aula capturando o tipo da variavel

--- main.py
+++ main.py
@@ -22,10 +22,25 @@ print(x + x)
 AULA DE FRASES 
 O Wesley tem 29 anos de idade e mora em São Paulo 
 '''
-nome = 'Wesley'
-idade = 32
-cidade = 'São Paulo'
-idade = str(idade)
 
+'''
+AULA DE INTERAÇÃO COM O PROGRAMA USANDO INPUT
+nome = input('Qual o seu nome: ')
+idade = input('Qual é a sua idade:' )
+cidade = input('Qual a sua cidade: ')
+idade = str(idade)
 print('O ' + nome + ' tem ' + idade + ' anos de idade e mora em ' + cidade)
+'''
+
+'''
+AULA DE CALCULAR A DATA ATRAVES DO ANO QUE NASCEU
+Data: 27/07/2023
+função type para saber o tipo de dado que está sendo apresentado
+'''
+
+ano_nascimento = 1993 
+ano_atual = 2023
+
+idade = int(ano_atual) - int(ano_nascimento)
+print('Wesley a sua idade é: ' + str(idade))
 
